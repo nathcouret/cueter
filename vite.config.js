2247a7f84cb1427a35a73b2b146018eda9ff5be4
Nathanael Couret <nathanael.couret@gmail.com>
2025-04-06 16:25:29 +0200

    github pages conf

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,8 @@ import vue from "@vitejs/plugin-vue";
 import tailwindcss from "@tailwindcss/vite";
 
 export default {
-    base: '/cueter/',
-    plugins: [vue(),
+    plugins: [
+        vue(),
         tailwindcss()
     ],
 }
